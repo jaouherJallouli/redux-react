63dbe5d998cd1f10349dc1e515e20d75f6ec9acd
jaouherJallouli <jaouherJlouli@gmail.com>
2021-08-27 10:35:27 +0200

    redux react

---
+++ src/Constants/action-types.js
@@ -0,0 +1,5 @@
+export const ADD_TODO = "ADD_TODO";
+export const COMPLETE_TODO = "COMPLETE_TODO";
+export const EDIT_TODO = "EDIT_TODO";
+// export const ACHIEVED_TODOS = "ACHIEVED_TODOS";
+// export const UNACHIEVED_TODOS = "UNACHIEVED_TODOS";
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./todo.css";
+import { completeTodo } from "../../Actions";
+import { useDispatch } from "react-redux";
+import EditTodo from "./EditTodo";
+const Todo = ({ task }) => {
+	const dispatch = useDispatch();
+	const completeHandler = () => {
+		dispatch(completeTodo({ id: task.id }));
+	};
+
+	return (
+		<div className='todo'>
+			<li className={`todo-item ${task.completed ? "completed" : ""}`}>
+				{task.text}
+			</li>
+			<div className='btn-container'>
+				<button
+					className={`${task.completed ? "notdone-btn" : "done-btn"}`}
+					onClick={completeHandler}
+				>
+					{`${task.completed ? "Not Done" : "Done"}`}
+				</button>
+				<EditTodo task={task} />
+			</div>
+		</div>
+	);
+};
+
+export default Todo;
